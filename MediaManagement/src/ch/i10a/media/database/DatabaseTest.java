2546f9b654e78caf57c640531b26aa3b542e7c2d
unknown <Fish-Guts@reefmaster.(none)>
2011-11-29 23:32:47 +0100

    updates database.class

---
+++ MediaManagement/src/ch/i10a/media/database/DatabaseTest.java
@@ -0,0 +1,16 @@
+package ch.i10a.media.database;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void test() {
+		
+		Database dataset = new Database();
+		fail("Not yet implemented");
+	}
+
+}
