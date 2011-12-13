b4365704a0a49669131bf6d77bd20f296d6f7510
Oliver Aeschbacher <aescholi@students.zhaw.ch>
2011-12-13 23:45:13 +0100

    Database connection test

--- MediaManagement/src/ch/i10a/media/database/DatabaseTest.java
+++ MediaManagement/src/ch/i10a/media/database/DatabaseTest.java
@@ -1,13 +1,36 @@
 package ch.i10a.media.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
 
 public class DatabaseTest {
 
 	@Test
-	public void test() {
+	public void testConnection() {
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = con.prepareStatement("select sysdate() from dual");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				System.out.println("Jupppppppieeeee: " + rs.getTime(1));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
-		Database dataset = new Database();
-		fail("Not yet implemented");
 	}
 
+	// ------------------ connection things ------------------
+	private Connection getConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		return DriverManager.getConnection( "jdbc:mysql://db4free.net:3306/mmanagement", "webmedia", "web.media");
+	}
 }
