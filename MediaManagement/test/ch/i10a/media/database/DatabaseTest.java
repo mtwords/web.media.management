291ad88657a163e62705c95dd61e2b976af1e5b1
Oliver Aeschbacher <aescholi@students.zhaw.ch>
2012-01-18 20:00:46 +0100

    JUnit: Leeren der DB als Test implementiert

--- MediaManagement/test/ch/i10a/media/database/DatabaseTest.java
+++ MediaManagement/test/ch/i10a/media/database/DatabaseTest.java
@@ -62,7 +62,191 @@ public class DatabaseTest {
 
 	@Test
 	public void deleteDBEntriesTest() {
-		
+		Connection connection = null;
+		ResultSet rs = null;
+		PreparedStatement psmnt = null;
+
+		try {
+			connection = getConnection();
+
+			int s;
+
+			// leeren von wrotescreenplay
+			//
+			psmnt = connection.prepareStatement("truncate wrotescreenplay");
+			psmnt.executeUpdate();
+			
+			// leeren von has
+			//
+			psmnt = connection.prepareStatement("truncate has");
+			s = psmnt.executeUpdate();
+			// leeren von language
+			//
+			psmnt = connection.prepareStatement("truncate language");
+			s = psmnt.executeUpdate();
+			// leeren von directs
+			//
+			psmnt = connection.prepareStatement("truncate directs");
+			s = psmnt.executeUpdate();
+			// leeren von actsin
+			//
+			psmnt = connection.prepareStatement("truncate actsin");
+			s = psmnt.executeUpdate();
+			// leeren von category
+			//
+			psmnt = connection.prepareStatement("truncate category");
+			s = psmnt.executeUpdate();
+			// leeren von rating
+			//
+			psmnt = connection
+					.prepareStatement("delete from rating where rating > 0");
+			s = psmnt.executeUpdate();
+			if (s > 0) {
+				System.out.println("loeschen von rating erfolgreich");
+			} else {
+				System.out
+						.println("loeschen von rating fehlgeschlagen oder Tabelle war leer.");
+			}
+			// leeren von person
+			//
+			psmnt = connection
+					.prepareStatement("delete from person where pID > 0");
+			s = psmnt.executeUpdate();
+			if (s > 0) {
+				System.out.println("loeschen von person erfolgreich");
+			} else {
+				System.out
+						.println("loeschen von person fehlgeschlagen oder Tabelle war leer.");
+			}
+			// leeren von episode
+			//
+			psmnt = connection
+					.prepareStatement("delete from episode where eID > 0");
+			s = psmnt.executeUpdate();
+			if (s > 0) {
+				System.out.println("loeschen von episode erfolgreich");
+			} else {
+				System.out
+						.println("loeschen von episode fehlgeschlagen oder Tabelle war leer.");
+			}
+			// leeren von movie
+			//
+			psmnt = connection
+					.prepareStatement("delete from movie where mID > 0");
+			s = psmnt.executeUpdate();
+			if (s > 0) {
+				System.out.println("loeschen von movie erfolgreich");
+			} else {
+				System.out
+						.println("loeschen von movie fehlgeschlagen oder Tabelle war leer.");
+			}
+
+			//
+			// Prüfen, ob die entsprechenden Tabellen leer sind
+			//
+
+			// Test für actsin
+			rs = psmnt.executeQuery("select * from actsin");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle actsin enthält noch Einträge");
+			} else {
+				System.out.println("actsin ist leer");
+			}
+			// Test für category
+			rs = psmnt.executeQuery("select * from category");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle category enthält noch Einträge");
+			} else {
+				System.out.println("category ist leer");
+			}
+			// Test für directs
+			rs = psmnt.executeQuery("select * from directs");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle directs enthält noch Einträge");
+			} else {
+				System.out.println("directs ist leer");
+			}
+			// Test für episode
+			rs = psmnt.executeQuery("select * from episode");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle episode enthält noch Einträge");
+			} else {
+				System.out.println("episode ist leer");
+			}
+			// Test für has
+			rs = psmnt.executeQuery("select * from has");
+
+			if (rs.next()) {
+				System.out.println("ERROR: Tabelle has enthält noch Einträge");
+			} else {
+				System.out.println("has ist leer");
+			}
+			// Test für language
+			rs = psmnt.executeQuery("select * from language");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle language enthält noch Einträge");
+			} else {
+				System.out.println("language ist leer");
+			}
+			// Test für movie
+			rs = psmnt.executeQuery("select * from movie");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle movie enthält noch Einträge");
+			} else {
+				System.out.println("movie ist leer");
+			}
+			// Test für person
+			rs = psmnt.executeQuery("select * from person");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle person enthält noch Einträge");
+			} else {
+				System.out.println("person ist leer");
+			}
+			// Test für rating
+			rs = psmnt.executeQuery("select * from rating");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle rating enthält noch Einträge");
+			} else {
+				System.out.println("rating ist leer");
+			}
+			// Test für wrotescreenplay
+			rs = psmnt.executeQuery("select * from wrotescreenplay");
+
+			if (rs.next()) {
+				System.out
+						.println("ERROR: Tabelle wrotescreenplay enthält noch Einträge");
+			} else {
+				System.out.println("wrotescreenplay ist leer");
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Found some error : " + ex);
+		} finally {
+			// close all the connections.
+			try {
+				connection.close();
+				psmnt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	@Test
